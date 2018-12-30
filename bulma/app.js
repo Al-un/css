d77bf745b3dde3b196de6b986c199162280bb37a
Al-un <alun.sng@gmail.com>
2018-12-30 23:20:22 +0100

    bulma content

--- bulma/app.js
+++ bulma/app.js
@@ -1,1 +1,47 @@
+import '../common/common.scss';
 import './app.scss';
+import * as Common from '../common';
+import './html/home.html';
+import './html/data.html';
+import './html/form.html';
+import './html/modal.html';
+import './html/notification.html';
+import './html/panel.html';
+
+const onHashChange = async () => {
+  await Common.onHashChange();
+};
+
+window.onhashchange = onHashChange;
+Common.loadDefaultPage();
+
+/**
+ * Bulma specific stuff
+ */
+const CSS_ACTIVE_MENU = 'is-active';
+
+/**
+ * When on mobile view, toggle menu content
+ */
+export const toggleMenu = () => {
+  document
+    // toggle menu content AND menu toggler burger
+    .querySelectorAll('.navbar > .container > .navbar-menu, .navbar-burger')
+    .forEach(node => {
+      node.classList.toggle(CSS_ACTIVE_MENU);
+    });
+};
+
+/**
+ * Popup a toast
+ */
+export const popNormalNotif = () => {
+  Common.toggleDisplay('notification-normal');
+};
+
+/**
+ * Popup an error toast
+ */
+export const popErrorNotif = () => {
+  Common.toggleDisplay('notification-error');
+};
