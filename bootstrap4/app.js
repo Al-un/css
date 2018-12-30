080a2de9ad47c4c08d6380d722ab2565f562071a
Al-un <alun.sng@gmail.com>
2018-12-30 12:57:58 +0100

    Bootstrap SPA-like

--- bootstrap4/app.js
+++ bootstrap4/app.js
@@ -1,1 +1,23 @@
+import '../common/common.scss';
 import './app.scss';
+import * as PageLoader from '../common/pageLoader';
+import './html/home.html';
+import './html/data.html';
+import './html/form.html';
+import './html/modal.html';
+import './html/notification.html';
+import './html/panel.html';
+
+const onHashChange = async () => {
+  const page = await PageLoader.onHashChange();
+  // Home page specific
+  console.log(`Loading page ${page}`);
+  if (page === 'home') {
+    document.getElementById('content').classList.remove('container');
+  } else {
+    document.getElementById('content').classList.add('container');
+  }
+};
+
+window.onhashchange = onHashChange;
+PageLoader.loadDefaultPage();
